d2905ccfbc2500890eb2989ee229ba8145891f55
Abhishek Gopalan <abhi4717@gmail.com>
2012-03-15 12:18:06 +0530

    I have implemented triggers and delegates in exercise 3

--- JQuery/Exercise 3/Javascript/script.js
+++ JQuery/Exercise 3/Javascript/script.js
@@ -1,8 +1,15 @@
 $(function() {
 // Bind highlight function on mouseover & mouseleave event on evtTarget element
 // unbind highlight function on click event on evtTarget element
-$("#evtTarget").bind('mouseover mouseout',highlight);
-$("#evtTarget").click(unbindMouseover);
+	$("#evtTarget").bind('mouseover mouseout',highlight);
+	$("#evtTarget").click(unbindMouseover);
+	$("#triggerButton").click(function(){
+			$("#evtTarget").trigger("mouseover");
+		});
+	$("#delegateButton").click(function (){
+		$("body").delegate("#txtEnterText","keydown", keydownAlert);
+	});
+
 });
 
 function highlight() {
@@ -13,3 +20,6 @@ function unbindMouseover(){
 $("#evtTarget").unbind();
 }
 
+function keydownAlert(){
+alert("Key pressed");
+}
